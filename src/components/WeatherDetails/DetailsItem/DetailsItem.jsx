eccff4b582c3cbed67530292f9c30f73bb130a80
Alex Grobovsky <ktotaam@mail.ru>
2021-09-13 17:13:44 +0500

    pixel perfect components styling

--- src/components/WeatherDetails/DetailsItem/DetailsItem.jsx
+++ src/components/WeatherDetails/DetailsItem/DetailsItem.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import s from './DetailsItem.module.scss';
+
 const DetailsItem = props => {
     return (
-        <h2>Details Item</h2>
+        <div className={s.detailsItem}>
+            <p className={s.detailsItem__title}>Details title</p>
+            <p className={s.detailsItem__desc}>Details description</p>
+        </div>
     )
-
 };
 
 export default DetailsItem;
