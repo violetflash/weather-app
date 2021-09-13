14ea2b15bf5e8c3c42a474d8f61058f5d131b28f
Alex Grobovsky <ktotaam@mail.ru>
2021-09-13 15:04:06 +0500

    feat: add base switcher styles

--- src/components/BaseSwitcher/BaseSwitcher.jsx
+++ src/components/BaseSwitcher/BaseSwitcher.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import s from './BaseSwitcher.module.scss';
+import { addConditionedStyle } from "../../utils/functions";
 
 const BaseSwitcher = props => {
+    const [activeBaseState, setActiveBaseState] = useState('C');
+
+    const switchHandler = (e) => {
+        setActiveBaseState(e.target.name);
+    };
+
+    const bases = ["C", "F"];
+    const switchButtons = bases.map((base) => {
+        const initialClass = base === "C" ? [s.switcher__celsius] : [s.switcher__fahrenheit];
+        const btnClass = addConditionedStyle(activeBaseState === base, initialClass, s.active);
+
+        return (
+            <button
+                key={base}
+                className={btnClass}
+                name={base}
+                onClick={switchHandler}
+            >
+                {base}
+            </button>
+        );
+    })
+
     return (
-        <h1>Switcher</h1>
+        <div className={s.switcher}>
+            {switchButtons}
+        </div>
+
     )
 
 };
---
+++ src/utils/functions.js
@@ -0,0 +1,60 @@
+const addConditionedStyle = (condition, actualClasses, newClass) => {
+    const classes = [...actualClasses];
+
+    if (condition) {
+        classes.push(newClass);
+    }
+
+    return classes.join(' ');
+};
+
+const checkImageExist = async (url) => {
+    const res = await fetch(url);
+    return !/source-404/.test(res.url);
+}
+
+const getArrayFromDb = (db) => {
+    const arr = [];
+    for (const key in db) {
+        arr.push({ ...db[key] });
+    }
+    return arr;
+};
+
+const addClass = (actualClass, newClass) => [actualClass, newClass].join(' ');
+
+const capitalizer = str => str[0].toUpperCase() + str.slice(1);
+
+
+const checkLS = (key, field, initial) => (
+    localStorage.getItem(key) ?
+        JSON.parse(localStorage.getItem(key))[field] :
+        initial
+);
+
+const getRandomIDsFromArr = (arr, num) => {
+    if (!arr) return [];
+    const newArr = new Set();
+    while (newArr.size !== num)  {
+        const elem = arr[Math.floor(Math.random() * arr.length)];
+        newArr.add(elem);
+    }
+
+    return Array.from(newArr);
+};
+
+const getFirstName = name => {
+    return name.split(' ')[0];
+}
+
+
+export {
+    addClass,
+    addConditionedStyle,
+    getArrayFromDb,
+    capitalizer,
+    checkLS,
+    getRandomIDsFromArr,
+    getFirstName,
+    checkImageExist
+};
