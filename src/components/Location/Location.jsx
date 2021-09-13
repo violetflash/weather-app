fe1a702f5e3cf5cca5ef14a089373288914a4519
Alex Grobovsky <ktotaam@mail.ru>
2021-09-13 16:43:01 +0500

    feat: add center weather comp styling

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import s from './Location.module.scss';
+
 const Location = props => {
+
+    const findLocationClass = [s.location__btn, s.location__find]
+
     return (
-        <h1>Выберите город</h1>
+        <div className={s.location}>
+            <h1 className={s.location__city}>Омск</h1>
+            <div className={s.location__controls}>
+                <button className={s.location__btn}>Сменить город</button>
+                <button className={findLocationClass.join(' ')}>Мое местоположение</button>
+            </div>
+        </div>
+
     )
 
 };
--- src/components/WeatherMainView/WeatherMainView.jsx
+++ src/components/WeatherMainView/WeatherMainView.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import s from './WeatherMainView.module.scss';
+import cloud from './icons/cloud.svg';
+import partlyCloudy from './icons/partly_cloudy.svg';
+import rain from './icons/rain.svg';
+import sun from './icons/sun.svg';
+import storm from './icons/storm.svg';
+
+
 const WeatherMainView = props => {
     return (
-        <h1>Main View</h1>
+        <div className={s.weather}>
+            <div className={s.weather__top}>
+                <img className={s.weather__icon} src={sun} alt=""/>
+                <span className={s.weather__temperature}>19</span>
+            </div>
+            <p className={s.weather__desc}>Преимущественно солнечно</p>
+        </div>
     )
 };
 
